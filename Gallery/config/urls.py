d9185f2cb479fbeb46fe3197c4661842225cff7c
Essael <urib4446@bears.unco.edu>
2024-12-02 16:31:04 -0700

    project 3

---
+++ Gallery/config/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from photos.views import PhotoView
+
+
+urlpatterns = [
+    path('', PhotoView.as_view()),
+]
+
---
+++ Gallery/photos/views.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class PhotoView(TemplateView):
+    template_name= 'photo.html'
